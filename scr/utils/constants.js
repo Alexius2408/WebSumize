7af66c187c4938c7a2204999b8b656a919fb6b75
Alexius <213401128+Alexius2408@users.noreply.github.com>
2026-03-12 12:20:52 +0100

    First Upload to GitHub

---
+++ scr/utils/constants.js
@@ -0,0 +1,14 @@
+// Central file for application-wide constants.
+// Each section groups related constants.
+
+
+
+// General application info
+const GENERALLY = {
+APP_NAME: 'WebSumize',
+APP_VERSION: '0.1.0-beta',
+}
+
+module.exports = {
+    GENERALLY
+}
