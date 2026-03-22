ae4538835485e303adc3602d8a168fb40edd0d7c
Alexius <213401128+Alexius2408@users.noreply.github.com>
2026-03-22 21:36:15 +0100

    added a logout button to the main menu

--- scr/renderer/mainWindow/index.js
+++ scr/renderer/mainWindow/index.js
@@ -9,14 +9,28 @@
  Description: This is the entry point for the main window. It shows the timetable for the current day.
 */
 
+const { ipcRenderer } = window.require("electron");
 
-const { ipcRenderer } = window.require('electron');
+const { delData } = require("../../services/storageService.js");
+
+const logoutbutton = document.getElementById("logoutBtn");
 
 async function loadTimetable() {
-    const timetable = await ipcRenderer.invoke('get-today-timetable');
-    const txt = document.createElement("p");
-    txt.textContent = JSON.stringify(timetable);
-    document.body.appendChild(txt);
+  const timetable = await ipcRenderer.invoke("get-today-timetable");
+  const txt = document.createElement("p");
+  txt.textContent = JSON.stringify(timetable);
+  document.body.appendChild(txt);
 }
+
+logoutbutton.addEventListener("click", async (event) => {
+  event.preventDefault();
+  await ipcRenderer.invoke("untis-logout");
+  await ipcRenderer.invoke("units-del-instance");
+  await delData();
+  await ipcRenderer.invoke(
+    "switch-window",
+    "../renderer/mainWindow/tabs/login/login.html",
+  );
+});
 
 loadTimetable();
